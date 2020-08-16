d7ca887d2ea1e52589d31d374cd97d67c9dba4e3
peeweetje <petravos70@gmail.com>
2020-08-16 16:37:15 +0200

    Add functionality to filter persons on name. Finish exercise 2.9

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -4,12 +4,21 @@ import Label from "./label";
 import "./app.css";
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState(
+    [
+      { name: "Arto Hellas", number: "040-123456" },
+      { name: "Ada Lovelace", number: "39-44-5323523" },
+      { name: "Dan Abramov", number: "12-43-234345" },
+      { name: "Mary Poppendieck", number: "39-23-6423122" },
+    ],
+  );
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterPersons, setFilterPersons] = useState("");
+  const [filteredPersons, setFilteredPersons] = useState(undefined);
 
-  const submitName = (event) => {
-    event.preventDefault();
+  const submitName = (e) => {
+    e.preventDefault();
 
     const filterDuplicateName = persons.filter(
       (person) => person.name === newName,
@@ -38,10 +47,28 @@ const App = () => {
     setNewNumber(e.target.value);
   };
 
+  const handleFilterPersons = (e) => {
+    setFilterPersons(e.target.value);
+    const filteredPersons = persons.filter((person) =>
+      person.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+
+    setFilteredPersons(filteredPersons);
+  };
+
   return (
     <>
       <h2>Phonebook</h2>
+      <Label htmlFor="search">
+        search
+        <FormInput
+          value={filterPersons}
+          onChange={handleFilterPersons}
+          type="text"
+        />
+      </Label>
       <form className="form-container" onSubmit={submitName}>
+        <h2>Add a new</h2>
         <Label htmlFor="name">
           Name:
           <FormInput value={newName} onChange={handleNewName} id="name" />
@@ -57,9 +84,19 @@ const App = () => {
         <button type="submit">add</button>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, i) => (
-        <li key={i}>{person.name} {person.number}</li>
-      ))}
+      {filterPersons === ""
+        ? persons?.map((person, i) =>
+          <li key={i}>
+            {person.name},
+            {person.number}
+          </li>
+        )
+        : filteredPersons?.map((person, i) => (
+          <li key={i}>
+            {person.name},
+            {person.number}
+          </li>
+        ))}
     </>
   );
 };
