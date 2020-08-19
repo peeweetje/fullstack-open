2d9f83bcca06e9414b4930fc7a1f843cc7ee5b49
peeweetje <petravos70@gmail.com>
2020-08-19 18:58:41 +0200

    Feature: add new component.

--- part2/the-phonebook/src/components/person-form.js
+++ part2/the-phonebook/src/components/person-form.js
@@ -1,8 +1,27 @@
 import React from "react";
+import Label from "../label";
+import FormInput from "../form-input";
 
-const PersonForms = () => {
+const PersonForms = (
+  { submitName, newName, handleNewName, newNumber, handleNewNumber },
+) => {
   return (
-    <div>hello</div>
+    <form className="form-container" onSubmit={submitName}>
+      <h2>Add a new</h2>
+      <Label htmlFor="name">
+        Name:
+        <FormInput value={newName} onChange={handleNewName} id="name" />
+      </Label>
+      <Label htmlFor="number">
+        Number:
+        <FormInput
+          value={newNumber}
+          onChange={handleNewNumber}
+          id="number"
+        />
+      </Label>
+      <button type="submit">add</button>
+    </form>
   );
 };
 
