da28206dd6ebc95ce4689d2a635a36b354375dcf
peeweetje <petravos70@gmail.com>
2020-08-28 17:10:37 +0200

    Feature: Add functionality to fetch data for the countries(w.i.p)

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Input from './components/input';
+import Label from './components/label';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://restcountries.eu/rest/v2/all').then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
+
+  const searchCountries = (e) => {
+    setCountries(e.target.value);
+    console.log(setCountries(e.target.value));
+  };
+
   return (
-    <div className='App'>
-      <h1>hello</h1>
+    <div>
+      <Label>Find countries</Label>
+      <Input onChange={searchCountries} />
     </div>
   );
 }
