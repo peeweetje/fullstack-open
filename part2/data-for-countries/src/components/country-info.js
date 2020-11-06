1f8d362d883820fc4bd494ad5b98da36bb13f3dd
peeweetje <petravos70@gmail.com>
2020-11-06 14:09:00 +0100

    Feature: create a new component for the info of the country.

---
+++ part2/data-for-countries/src/components/country-info.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const countryInfo = ({country}) => {
+    return (
+        <>
+         <h1>{country.name}</h1>
+          <span>Capital: {country.capital}</span>
+          <span>Population: {country.population}</span>
+
+         <h2>Languages</h2> 
+           {country?.languages.map(language => (
+             <li key={language.name}>{language.name}</li>
+            ))}
+             <img
+              src={country.flag}
+              alt={`Flag of ${country.name}`}
+              width="100px"
+              height="100px"
+            />
+       </>
+    )
+}
+
+export default countryInfo;
