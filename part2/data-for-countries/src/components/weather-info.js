ffa8f5c78867aadede3d2c01f1828d779acf869a
peeweetje <petravos70@gmail.com>
2021-01-22 12:28:36 +0100

    finish exercise 2.14

--- part2/data-for-countries/src/components/weather-info.js
+++ part2/data-for-countries/src/components/weather-info.js
@@ -3,24 +3,25 @@ import React from "react";
 const WeatherInfo = ({ weatherData }) => (
   <>
     {weatherData && (
-      <>
+      <div>
         <h2>Weather in {weatherData?.location.name}</h2>
-        <>
-          <span >Temperature: </span>
-          <span>{weatherData?.current.temperature} &deg; Celsius</span>
-        </>
-        <>
+        <div>
           <img
             src={weatherData?.current.weather_icons[0]}
             alt="symbol for showing the weather"
           />
-        </>
-        <>
+        </div>
+        <div>
+          <span >Temperature: </span>
+          <span>{weatherData?.current.temperature} &deg; Celsius</span>
+        </div>
+     
+        <div>
           <span >Wind: </span>
           <span>{weatherData?.current.wind_speed} mph </span>
           <span>direction {weatherData?.current.wind_dir}</span>
-        </>
-      </>
+        </div>
+      </div>
     )}
   </>
 );
